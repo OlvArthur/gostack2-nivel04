b67bb0b147da3f2a96cc2a472fcdec46bbec4d6e
Arthur Oliveira <olvarthur@gmail.com>
2020-05-06 23:39:27 -0300

    Improving tests

--- arquitetura-e-testes-node.js/aulas/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ arquitetura-e-testes-node.js/aulas/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -18,7 +20,25 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('485793874589');
   });
 
-  // it('should not be able to create two appointments at the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments at the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '485793874589',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '485793874589',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
