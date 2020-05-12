0415eeefcb40cd05b301fdebb7e22d8bfa812a20
Arthur Oliveira <olvarthur@gmail.com>
2020-05-12 20:15:56 -0300

    Implementing provider availability in each day

---
+++ arquitetura-e-testes-node.js/aulas/src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -0,0 +1,45 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
+
+let listProviderDayAvailabilityService: ListProviderDayAvailabilityService;
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderDayAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderDayAvailabilityService = new ListProviderDayAvailabilityService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list a provider`s day availability ', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    });
+
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 21, 12, 0, 0),
+    });
+
+    const availability = await listProviderDayAvailabilityService.execute({
+      provider_id: 'user',
+      day: 21,
+      year: 2020,
+      month: 5,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 10, available: true },
+        { hour: 12, available: false },
+        { hour: 14, available: true },
+        { hour: 16, available: true },
+      ]),
+    );
+  });
+});
